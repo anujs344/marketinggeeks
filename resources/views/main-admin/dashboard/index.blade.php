ba4894eb542d401a2a644c5e4f5f3c445c1b3a75
bwf1dodolo <82048279+bwf1dodolo@users.noreply.github.com>
2022-09-14 14:52:40 +0530

    verygoodcommit

--- resources/views/main-admin/dashboard/index.blade.php
+++ resources/views/main-admin/dashboard/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>MarketingGeeks</title>
-  <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('plugins/fontawesome-free/css/all.min.css')}}">
 
   @yield('css')
 
@@ -14,7 +14,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{secure_asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   @include('main-admin.dashboard.layouts.navbar')
